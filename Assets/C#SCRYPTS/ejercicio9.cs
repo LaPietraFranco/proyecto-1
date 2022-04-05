bd1d5182c9f4815a47acffb4acdbe96da0f4aec7
LaPietraFranco <101827023+LaPietraFranco@users.noreply.github.com>
2022-04-05 14:10:25 -0300

    test for exam commit

--- Assets/C#SCRYPTS/ejercicio9.cs
+++ Assets/C#SCRYPTS/ejercicio9.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ejercicio9 : MonoBehaviour
-{ public int persona1;
-    public int persona2;
-    public int persona3;
-    public string nombre1;
-    public string nombre2;
-    public string nombre3;
+{ public string persona1;
+    public string persona2;
+    public string persona3;
+    public float capitalpersona3;
+    public float capitalpersona1;
+    public float capitalpersona2;
+    float porcentaje1;
+    float porcentaje2;
+    float porcentaje3;
+    float montototal;
+
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Nombre:" + persona1 + "capital aportado: $" + capitalpersona1 + "Porcentaje del capital: %" + porcentaje1 + "Monto total aportado: $" + montototal);
 
-    }
+}
 
     // Update is called once per frame
     void Update()
