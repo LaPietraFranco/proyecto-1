d491d136946b0b74362dc317526a7a0000228143
LaPietraFranco <101827023+LaPietraFranco@users.noreply.github.com>
2022-04-05 14:25:04 -0300

    excercise 9 completed

--- Assets/C#SCRYPTS/ejercicio9.cs
+++ Assets/C#SCRYPTS/ejercicio9.cs
@@ -17,9 +17,14 @@ public class ejercicio9 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        montototal = capitalpersona1 + capitalpersona2 + capitalpersona3;
+        porcentaje1 = capitalpersona1 / montototal * 100;
+        porcentaje2 = capitalpersona2 / montototal * 100;
+        porcentaje3 = capitalpersona3 / montototal * 100;
         Debug.Log("Nombre:" + persona1 + "capital aportado: $" + capitalpersona1 + "Porcentaje del capital: %" + porcentaje1 + "Monto total aportado: $" + montototal);
-
-}
+        Debug.Log("Nombre:" + persona2 + "capital aportado: $" + capitalpersona2 + "Porcentaje del capital: %" + porcentaje2 + "Monto total aportado: $" + montototal);
+        Debug.Log("Nombre:" + persona3 + "capital aportado: $" + capitalpersona3 + "Porcentaje del capital: %" + porcentaje3 + "Monto total aportado: $" + montototal);
+    }
 
     // Update is called once per frame
     void Update()
